9c602ac953f40e0ddc6bc8d0f1a6d8cd646971aa
Anshul-Sharma12 <anshulsharma052005@gmail.com>
2024-08-07 18:37:26 +0530

    cookie on cart

--- test.js
+++ test.js
@@ -1,63 +1,50 @@
-// let decodedCookie = "0" + decodeURIComponent(document.cookie);
-// console.log(decodedCookie.split(","));
-
 function setcookie(input) {
-    var a = "product-id";
     var cookie_value = document.cookie;
-    document.cookie = cookie_value + "," + input;
-    var prod_id = document.cookie ;
-    console.log("product_ids" + "=" + document.cookie);
+    document.cookie = cookie_value + encodeURIComponent(input) + ", ";
+    console.log("product_ids" + " " + "=" + " " + (document.cookie).replace(/,$/, ''));
     return document.cookie;
 }
-let prod_arr;
+
+let prod_arr = [];
 function removeduplicates(data) {
-    prod_arr = [...new Set(data)];
+    for (let i = 0; i < data.length; i++) {
+        if (prod_arr.indexOf(data[i]) === -1) {
+            prod_arr.push(data[i]);
+        }
+    }
     return prod_arr;
 }
-console.log(removeduplicates(document.cookie));
+console.log(removeduplicates((document.cookie).replace(/,$/, '').split(',')));
 
 let product_array = [];
-product_array[1] = ["./prod1.jpg, OnePlus Nord CE4 Lite 5G"];
-product_array[2] = ["./prod2.jpg, Samsung Galaxy M35 5G"];
-product_array[3] = ["./prod3.jpg, iQOO Z9x 5G"];
-product_array[4] = ["./prod4.jpg, realme NARZO 70 5G"];
-product_array[5] = ["./prod5.jpg, Redmi 12 5G"];
-product_array[6] = ["./prod6.jpg, Samsung Galaxy S24 Ultra 5G"];
-product_array[7] = ["./prod7.jpg, OnePlus 12R"];
-product_array[8] = ["./prod8.jpg, iQOO Z9 5G"];
-product_array[9] = ["./prod9.jpg, Samsung Galaxy S23 Ultra 5G"];
-// // console.log(product_array);
-
-// function getcookie() {
-//     for (i = 0; i < product_array.length ; i++);
-//     if (document.cookie = i) {
-//         document.getElementById("demo").innerHTML = product_array[i];
-//     }
-// }
+product_array[1] = ["./prod1.jpg", "OnePlus Nord CE4 Lite 5G"];
+product_array[2] = ["./prod2.jpg", "Samsung Galaxy M35 5G"];
+product_array[3] = ["./prod3.jpg", "iQOO Z9x 5G"];
+product_array[4] = ["./prod4.jpg", "realme NARZO 70 5G"];
+product_array[5] = ["./prod5.jpg", "Redmi 12 5G"];
+product_array[6] = ["./prod6.jpg", "Samsung Galaxy S24 Ultra 5G"];
+product_array[7] = ["./prod7.jpg", "OnePlus 12R"];
+product_array[8] = ["./prod8.jpg", "iQOO Z9 5G"];
+product_array[9] = ["./prod9.jpg", "Samsung Galaxy S23 Ultra 5G"];
 
 function getcookie() {
-    // Split the cookie string and extract the product IDs
-    let cookies = document.cookie.split('; ').find(row => row.startsWith('product-id='));
-    if (cookies) {
-        let productIDs = cookies.split('=')[1].split(','); // Get the IDs from the cookie
-        let uniqueIDs = removeduplicates(productIDs);
-        
-        // Display the product information based on the IDs
-        let productDisplay = uniqueIDs.map(id => {
-            let index = parseInt(id); // Convert string ID to integer index
-            if (product_array[index]) {
-                return `<img src="${product_array[index][0]}" alt="${product_array[index][1]}"> ${product_array[index][1]}`;
-            }
-            return ''; // Return an empty string for non-existing indices
-        }).join('<br/>'); // Join the results with line breaks
-        demo.innerHTML = productDisplay; // Display products in demo element
-    } else {
-        demo.innerHTML = "No products selected.";
+    for (let i = 0; i < prod_arr.length; i++) {
+        const productContainer = document.getElementById('product-container');
+        const a = prod_arr[i];
+        const b = product_array[a];
+        // console.log(b);
+        const productElement = document.createElement('div');
+        productElement.className = 'product';
+        productElement.innerHTML = `
+                <img src="${b[0]}" alt="${b[1]}" />
+                <p>${b[1]}</p>
+                <button onclick="removecookie()">Remove from Cart</button>
+            `;
+        productContainer.appendChild(productElement);
     }
 }
-
+getcookie();
 
 function removecookie() {
-    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; 
-    console.log(document.cookie)
+    
 }
