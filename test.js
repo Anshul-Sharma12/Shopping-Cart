3cf4ce40e69a8345d3229ca9d2f9824d5186b86f
Anshul-Sharma12 <anshulsharma052005@gmail.com>
2024-08-09 10:44:43 +0530

    cookie on cart

--- test.js
+++ test.js
@@ -30,21 +30,79 @@ product_array[9] = ["./prod9.jpg", "Samsung Galaxy S23 Ultra 5G"];
 function getcookie() {
     for (let i = 0; i < prod_arr.length; i++) {
         const productContainer = document.getElementById('product-container');
-        const a = prod_arr[i];
-        const b = product_array[a];
-        // console.log(b);
+        const prod_element = prod_arr[i];
+        const main_prod = product_array[prod_element];
         const productElement = document.createElement('div');
         productElement.className = 'product';
+        productElement.id = prod_element;
         productElement.innerHTML = `
-                <img src="${b[0]}" alt="${b[1]}" />
-                <p>${b[1]}</p>
-                <button onclick="removecookie()">Remove from Cart</button>
+                <img src="${main_prod[0]}" alt="${main_prod[1]}" />
+                <p>${main_prod[1]}</p>
+                <button onclick= removecookie('${productElement.id}')>Remove from Cart</button>
             `;
         productContainer.appendChild(productElement);
     }
 }
 getcookie();
 
-function removecookie() {
-    
+function removecookie(input) {
+    var cookie_details = document.cookie;
+    document.cookie = cookie_details.replace(input, " ");
+    console.log("final ans = " + document.cookie.replace(/,/, ''));
+    return document.cookie;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     // const index = prod_arr.indexOf(data);
+//     // if (index > -1) {
+//     //     prod_arr.splice(index, 1);
+//     // }
+//     // console.log(prod_arr);
+//     for (let i = 0; i < prod_arr.length; i++) {
+//     var cookie_details = (document.cookie).includes("1");
+//     // console.log(cookie_details);
+//     if (cookie_details == true) {
+//         cookie_details = (document.cookie).replace("1,", " ");
+//         console.log(cookie_details);
+//         return cookie_details;
+//     }else {
+//         return cookie_details;
+//     }
+//     // const str_index = cookie_details.indexOf(data);
+//     // if (str_index > -1) {
+//     //     cookie_details.replace(`${str_index},`, " ");
+//     // }
+//     // console.log(`Final cookine value : ${cookie_details}`)
+// }}
+
+// function removecookie(data) {
+//     const productElement = data.parentElement;
+
+//     const productId = productElement.id;
+
+//     let cookie_value = document.cookie ? decodeURIComponent(document.cookie) : '';
+//     let cookieArray = cookie_value.split(', ').filter(Boolean);
+//     cookieArray = cookieArray.filter(id => id !== productId);
+//     console.log(cookieArray);
+
+//     document.cookie = encodeURIComponent(cookieArray.join(', ')) + '; path=/';
+
+//     productElement.remove();
+// }
+
+// window.onload = function () {
+//     getcookie();
+// };
+
